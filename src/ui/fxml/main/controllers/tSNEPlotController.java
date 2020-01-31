12e156e1af3e1f11e2eca09537b603a31e2191ad
msteph11 <maria.stephenson235@gmail.com>
2020-01-31 11:39:08 -0800

    removed SMILE library

--- src/ui/fxml/main/controllers/tSNEPlotController.java
+++ src/ui/fxml/main/controllers/tSNEPlotController.java
@@ -7,30 +7,19 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
-import org.apache.commons.csv.CSVFormat;
-import smile.data.DataFrame;
-import smile.io.Read;
-import smile.manifold.TSNE;
-import smile.math.MathEx;
-import smile.plot.swing.Palette;
-import smile.plot.swing.PlotCanvas;
-import smile.projection.PCA;
 
 import javax.swing.*;
 import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class tSNEPlotController implements Runnable, Initializable {
+public class tSNEPlotController implements Initializable {
     @FXML private VBox tSNEPlot;
     @FXML private Button drawTSNEButton;
     @FXML private TextField perplexity;
     @FXML private SwingNode canvas;
 
     private JPanel pane;
-    private double[][] data;
-    private int[] labels;
-    private char pointLegend = '@';
 
     public VBox getTSNEPlot() {
         return tSNEPlot;
@@ -41,68 +30,9 @@ public class tSNEPlotController implements Runnable, Initializable {
      */
     @FXML
     protected void drawTSNE(ActionEvent e) {
-        try {
-            CSVFormat format = CSVFormat.DEFAULT.withDelimiter(' ').withIgnoreSurroundingSpaces(true);
-            URL url = getClass().getResource("../../../../sampledata/mnist2500_X.txt");
-            DataFrame dataset = Read.csv(java.nio.file.Paths.get(url.toURI()), format);
-            this.data = dataset.toArray();
-            URL url2 = getClass().getResource("../../../../sampledata/mnist2500_labels.txt");
-            dataset = Read.csv(java.nio.file.Paths.get(url2.toURI()));
-            this.labels = dataset.column(0).toIntArray();
-        } catch (Exception var4) {
-            JOptionPane.showMessageDialog(null, "Failed to load dataset.", "ERROR", JOptionPane.ERROR_MESSAGE);
-            System.err.println(var4.getMessage());
-        }
-
-        try {
-            int perplexityValue = Integer.parseInt(perplexity.getText().trim());
-            if (perplexityValue < 10 || perplexityValue > 300) {
-                System.out.println("TODO");
-                return;
-            }
-        } catch (Exception var5) {
-            System.out.println("TODO");
-            return;
-        }
-
-        Thread thread = new Thread(this);
-        thread.start();
+        System.out.println("TODO");
     }
 
-    public PlotCanvas learn() {
-        PCA pca = PCA.fit(this.data);
-        pca.setProjection(50);
-        double[][] X = pca.project(this.data);
-        long clock = System.currentTimeMillis();
-        TSNE tsne = new TSNE(X, 2, Double.parseDouble(perplexity.getText()), 200.0D, 1000);
-        System.out.format("Learn t-SNE from %d samples in %dms\n", this.data.length, System.currentTimeMillis() - clock);
-        double[][] y = tsne.coordinates;
-        PlotCanvas plot = new PlotCanvas(MathEx.colMin(y), MathEx.colMax(y));
-
-        for(int i = 0; i < y.length; ++i) {
-            plot.point(this.pointLegend, Palette.COLORS[this.labels[i]], y[i]);
-        }
-        plot.setTitle("t-SNE");
-        return plot;
-    }
-
-    public void run() {
-        drawTSNEButton.setDisable(true);
-        perplexity.setDisable(true);
-
-        try {
-            PlotCanvas plot = this.learn();
-            if (plot != null) {
-                pane.add(plot);
-                pane.validate();
-            }
-        } catch (Exception var2) {
-            System.err.println(var2.getMessage());
-        }
-
-        drawTSNEButton.setDisable(false);
-        perplexity.setDisable(false);
-    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
